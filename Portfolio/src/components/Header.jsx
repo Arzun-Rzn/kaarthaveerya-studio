3592be8dbfc7997865d394a43d57eba1d8f7e3b3
Nagarjuna <nagarjunajogi08@gmail.com>
2025-04-26 00:27:51 +0530

    Fixed logo

--- Portfolio/src/components/Header.jsx
+++ Portfolio/src/components/Header.jsx
@@ -4,7 +4,7 @@ import '../styles/header.css';
 const Header = () => {
   return (
     <div className="title">
-       <img src="../public/images/kaarthaveerya.png" alt="Logo" className="logo" />
+       <img src="../src/assets/kaarthaveerya.jpg" alt="Logo" className="logo" />
        <h1>KAARTHAVEERYA STUDIO</h1>
     </div>
   )
