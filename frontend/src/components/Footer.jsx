e317a33837468ebc275d133f5427fa5a5e20a5d1
Nagarjuna <nagarjunajogi08@gmail.com>
2026-04-01 11:59:09 +0530

    fixed footer socials

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+import {
+  FaInstagram,
+  FaFacebook,
+  FaYoutube,
+  FaPinterest,
+  FaReddit,
+  FaTumblr,
+  FaBehance
+} from "react-icons/fa";
+
+import { MdEmail } from "react-icons/md";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+
 import '../styles/footer.css';
 
 const Footer = () => {
@@ -27,21 +42,51 @@ const Footer = () => {
 
       <div className="footer-social">
         <h4>CONNECT</h4>
-        <a
-          href="mailto:kaarthaveeryaa.arjuna@gmail.com"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Gmail
+
+        <a href="mailto:kaarthaveeryaa.arjuna@gmail.com" target="_blank" rel="noreferrer" data-name="Gmail">
+          <MdEmail />
         </a>
-        <a href="https://www.instagram.com/kaarthaveerya/" target="_blank" rel="noreferrer">Instagram</a>
-        <a href="https://www.facebook.com/profile.php?id=61575235523785" target="_blank" rel="noreferrer">Facebook</a>
-        <a href="https://x.com/kaarthaveerrya" target="_blank" rel="noreferrer">X (Twitter)</a>
-        <a href="https://www.youtube.com/@KaarthaveeryaArjuna" target="_blank" rel="noreferrer">YouTube</a>
-        <a href="https://www.behance.net/KaarthaveeryaArjun" target="_blank" rel="noreferrer">Behance</a>
-        <a href="https://in.pinterest.com/kaarthaveeryaa/_created/" target="_blank" rel="noreferrer">Pinterest</a>
-        <a href="https://www.reddit.com/u/kaarthaveerya/s/7MnUIMvA8D" target="_blank" rel="noreferrer">Reddit</a>
-        <a href="https://www.tumblr.com/blog/kaarthaveerya-arjun" target="_blank" rel="noreferrer">Tumblr</a>
+
+        <a href="https://www.instagram.com/kaarthaveerya/" target="_blank" rel="noreferrer" data-name="Instragram">
+          <FaInstagram />
+        </a>
+
+        <a href="https://www.facebook.com/profile.php?id=61575235523785" target="_blank" rel="noreferrer" data-name="Facebook">
+          <FaFacebook />
+        </a>
+
+        <a href="https://x.com/kaarthaveerrya" target="_blank" rel="noreferrer" data-name="X(Twitter)">
+          <FaXTwitter />
+        </a>
+
+        <a href="https://www.youtube.com/@KaarthaveeryaArjuna" target="_blank" rel="noreferrer" data-name="Youtube">
+          <FaYoutube />
+        </a>
+
+        <a href="https://www.behance.net/KaarthaveeryaArjun" target="_blank" rel="noreferrer" data-name="Behance">
+          <FaBehance />
+        </a>
+
+        <a href="https://in.pinterest.com/kaarthaveeryaa/_created/" target="_blank" rel="noreferrer" data-name="Pinterest">
+          <FaPinterest />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/YOUR-USERNAME/"
+          target="_blank"
+          rel="noreferrer"
+          data-name="LinkedIn"
+        >
+          <FaLinkedin />
+        </a>
+
+        <a href="https://www.reddit.com/u/kaarthaveerya/s/7MnUIMvA8D" target="_blank" rel="noreferrer" data-name="Reddit">
+          <FaReddit />
+        </a>
+
+        <a href="https://www.tumblr.com/blog/kaarthaveerya-arjun" target="_blank" rel="noreferrer" data-name="Tumblr">
+          <FaTumblr />
+        </a>
       </div>
 
       <div className="footer-bottom">
